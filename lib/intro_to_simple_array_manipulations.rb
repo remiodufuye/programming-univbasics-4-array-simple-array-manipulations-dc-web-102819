5e91cea6040a27ffda6eb2dfc7a0964eda59bd38
remi.odufuye <remi.odufuye@gmail.com>
2019-10-14 12:25:43 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,58 @@
+
+
+# artists = ["Hozier", "Ariana Grande", "Usher"]
+
+def using_push(array , string )
+  array.push(string) 
+end 
+
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array) 
+  # Please Review
+  array.pop(2)
+end 
+
+def using_shift(array)
+  first = array.shift
+  p first 
+end 
+
+def shift_with_args(array)
+   array.shift(2)
+end 
+
+def using_concat(arg1,arg2) 
+  concat(arg1,arg2) 
+end  
+
+def using_insert(array,element)
+  array.insert(4, element)  
+end 
+
+def using_uniq(array)
+  array.uniq 
+end 
+
+def using_flatten(array)
+  array.flatten
+end 
+
+def using_delete (array , string)
+  array.delete(string)
+end 
+
+def using_delete_at(array,integer)
+  array.delete_at(integer) 
+end 
+
+
+
+
+
